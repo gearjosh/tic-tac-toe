f922c5dd6d866f9372a3996166d39221b2088fe8
Dan Mace and Josh Gearheart <dan@>
2018-08-22 11:49:12 -0700

    add some barely working methods

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,57 @@
 function Player(){
-  this.playerNumber = NaN;
+  this.playerName = "";
   this.mark = "";
 }
 
 function Game() {
   this.players = [];
+  this.activePlayer = "player1"
 }
+
+function Board() {
+  this.row1 = {
+    square1 : {
+      position : [0,0],
+      ownership : "unclaimed"
+    },
+    square2 : {
+      position : [0,1],
+      ownership : "unclaimed"
+    },
+    square3 : {
+      position : [0,2],
+      ownership : "unclaimed"
+    }
+  },
+  this.row2 = {
+    square1 : {
+      position : [1,0],
+      ownership : "unclaimed"
+    },
+    square2 : {
+    position : [1,1],
+    ownership : "unclaimed"
+    },
+    square3 : {
+      position : [1,2],
+      ownership : "unclaimed"
+    }
+  },
+  this.row3 = {
+    square1 : {
+    position : [2,0],
+    ownership : "unclaimed"
+  },
+    square2 : {
+      position : [2,1],
+      ownership : "unclaimed"
+    },
+    square3 : {
+      position : [2,2],
+      ownership : "unclaimed"
+    }
+  };
+};
 
 Game.prototype.playerLoad = function() {
   var player1 = new Player();
@@ -17,7 +63,7 @@ Game.prototype.playerLoad = function() {
   var playerCount = 1;
 
   this.players.forEach(function(playerObject) {
-    playerObject.playerNumber = playerCount;
+    playerObject.playerName = "player" + playerCount;
     if (playerCount === 1) {
       playerObject.mark = "X";
     }
@@ -28,8 +74,56 @@ Game.prototype.playerLoad = function() {
   });
 }
 
-var newGame = new Game();
+Game.prototype.turnTaker = function() {
+  var currentPlayer = this.activePlayer;
+  if (currentPlayer == "player1") {
+    this.activePlayer = this.players[1].playerName;
+  } else {
+    this.activePlayer = this.players[0].playerName;
+  }
+  console.log(currentPlayer);
+};
 
+Board.prototype.markSquare = function(square, playerObject) {
+  var position = square.position;
+  var mark = playerObject.mark;
+  if (mark == "X") {
+    square.ownership = "player1";
+    console.log(square);
+    return "HIT";
+  } else {
+    square.ownership = "player2";
+    console.log(square);
+  }
+};
+
+function arrayCoords(id) {
+  var coordinates = id.split("")
+  var coordinates = coordinates.map(function(value) {
+    return parseInt(value);
+  })
+  return coordinates;
+};
+
+
+
+
+//Game Logic Begins
+var newGame = new Game();
 newGame.playerLoad();
 
-console.log(newGame)
+var newBoard = new Board();
+
+
+
+$(function () {
+  $(".gamesquare").click(function() {
+    var squareSelected = $(this).attr('id');
+    console.log(squareSelected);
+    var arrayedInput = arrayCoords(squareSelected);
+    var loggedMark = newBoard.markSquare(arrayedInput, newGame.players[0]);
+    console.log(loggedMark)
+
+    newGame.turnTaker();
+  });
+});
